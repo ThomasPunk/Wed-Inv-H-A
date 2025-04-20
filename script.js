c51cf592e7401cc6bd9f128b8939c3a664972462
ThomasPunk <virus.arief@gmail.com>
2025-04-21 07:31:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -130,4 +130,15 @@ function openPage(){
   container.classList.remove('hidden');  
   const audio = document.getElementById("soundTrack");
     audio.play();
+
+  let elem = document.documentElement; // atau bisa juga elemen spesifik seperti document.getElementById('container')
+  
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+
 } 
